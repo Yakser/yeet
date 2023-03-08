373500f7b52badefe67797ed5e85253f24e2e48e
Yakser <iaksanov2044@mail.ru>
2023-03-08 13:23:21 +0300

    Fixed app_name attribute

--- examples/main.py
+++ examples/main.py
@@ -1,12 +1,12 @@
 import os
 
-from examples.controllers import HomeController, ProfileController
+from controllers import HomeController, ProfileController
 from python_yeet.app import Yeet
 
-app = Yeet(name=os.path.basename(os.getcwd()))
-
-app.add_route('^home/profile(.*)$', ProfileController)
-app.add_route('^home/(.*)$', HomeController)
-
 if __name__ == '__main__':
+    app = Yeet(name=os.path.basename(os.getcwd()))
+
+    app.add_route('^home/profile(.*)$', ProfileController)
+    app.add_route('^home/(.*)$', HomeController)
+
     app.run(host='', port=8080)
